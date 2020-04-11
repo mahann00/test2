0bc9b6de7c80b2b06b5123e2da53d657ec0c1699
mahann00 <your_email@mahanmohamadi.1382@gmail.com>
2020-04-12 00:06:45 +0430

    Readme.txt

---
+++ calculator00/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace calculator00
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblnumber1 = new System.Windows.Forms.Label();
+            this.lbhnumber2 = new System.Windows.Forms.Label();
+            this.txtnumber1 = new System.Windows.Forms.NumericUpDown();
+            this.txtnumber2 = new System.Windows.Forms.NumericUpDown();
+            this.btnplus = new System.Windows.Forms.Button();
+            this.btnmultiple = new System.Windows.Forms.Button();
+            this.btnminus = new System.Windows.Forms.Button();
+            this.btndivide = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.txtnumber1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtnumber2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblnumber1
+            // 
+            resources.ApplyResources(this.lblnumber1, "lblnumber1");
+            this.lblnumber1.Name = "lblnumber1";
+            this.lblnumber1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbhnumber2
+            // 
+            resources.ApplyResources(this.lbhnumber2, "lbhnumber2");
+            this.lbhnumber2.Name = "lbhnumber2";
+            this.lbhnumber2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtnumber1
+            // 
+            resources.ApplyResources(this.txtnumber1, "txtnumber1");
+            this.txtnumber1.Name = "txtnumber1";
+            // 
+            // txtnumber2
+            // 
+            resources.ApplyResources(this.txtnumber2, "txtnumber2");
+            this.txtnumber2.Name = "txtnumber2";
+            this.txtnumber2.ValueChanged += new System.EventHandler(this.txtnumber2_ValueChanged);
+            // 
+            // btnplus
+            // 
+            resources.ApplyResources(this.btnplus, "btnplus");
+            this.btnplus.Name = "btnplus";
+            this.btnplus.UseVisualStyleBackColor = true;
+            this.btnplus.Click += new System.EventHandler(this.btnplus_Click);
+            // 
+            // btnmultiple
+            // 
+            resources.ApplyResources(this.btnmultiple, "btnmultiple");
+            this.btnmultiple.Name = "btnmultiple";
+            this.btnmultiple.UseVisualStyleBackColor = true;
+            this.btnmultiple.Click += new System.EventHandler(this.btnmultiple_Click);
+            // 
+            // btnminus
+            // 
+            resources.ApplyResources(this.btnminus, "btnminus");
+            this.btnminus.Name = "btnminus";
+            this.btnminus.UseVisualStyleBackColor = true;
+            this.btnminus.Click += new System.EventHandler(this.btnminus_Click);
+            // 
+            // btndivide
+            // 
+            resources.ApplyResources(this.btndivide, "btndivide");
+            this.btndivide.Name = "btndivide";
+            this.btndivide.UseVisualStyleBackColor = true;
+            this.btndivide.Click += new System.EventHandler(this.btndivide_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btndivide);
+            this.Controls.Add(this.btnminus);
+            this.Controls.Add(this.btnmultiple);
+            this.Controls.Add(this.btnplus);
+            this.Controls.Add(this.txtnumber2);
+            this.Controls.Add(this.txtnumber1);
+            this.Controls.Add(this.lbhnumber2);
+            this.Controls.Add(this.lblnumber1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.txtnumber1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtnumber2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblnumber1;
+        private System.Windows.Forms.Label lbhnumber2;
+        private System.Windows.Forms.NumericUpDown txtnumber1;
+        private System.Windows.Forms.NumericUpDown txtnumber2;
+        private System.Windows.Forms.Button btnplus;
+        private System.Windows.Forms.Button btnmultiple;
+        private System.Windows.Forms.Button btnminus;
+        private System.Windows.Forms.Button btndivide;
+    }
+}
+
---
+++ calculator00/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator00
+{
+    public partial class Form1 : Form
+    {
+        IMycalculator calculator;
+        public Form1()
+        {
+            InitializeComponent();
+            calculator = new Calculator();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnplus_Click(object sender, EventArgs e)
+        {
+            if(validateinpot())
+            {
+                int plus = calculator.plus((int)txtnumber1.Value ,(int) txtnumber2.Value);
+                MessageBox.Show("int sum :"+plus);
+            }
+        }
+
+        private void txtnumber2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        bool validateinpot()
+
+        {
+            bool itvalidate = true;
+            if (txtnumber1.Value == 0)
+            {
+                MessageBox.Show("عدد صفر وارد نکن");
+                itvalidate = false;
+
+            }
+            else
+            {
+
+
+                if (txtnumber2.Value == 0)
+                {
+                    MessageBox.Show("گمج دومی رو هم صفر وارد نکن ");
+                    itvalidate = false;
+                }
+            }
+
+           return itvalidate;
+        }
+
+        private void btnmultiple_Click(object sender, EventArgs e)
+        {
+            if(validateinpot())
+            {
+                int multiple = calculator.multiple((int)txtnumber1.Value, (int)txtnumber2.Value);
+                MessageBox.Show("int multiple:" + multiple);
+            }
+        }
+
+        private void btnminus_Click(object sender, EventArgs e)
+        {
+            if(validateinpot())
+            {
+                int minus = calculator.minus((int)txtnumber1.Value, (int)txtnumber2.Value);
+                MessageBox.Show("int minus:" + minus);
+            }
+
+        }
+
+        private void btndivide_Click(object sender, EventArgs e)
+        {
+            int divide = calculator.divide((int)txtnumber1.Value, (int)txtnumber2.Value);
+
+            MessageBox.Show("int divide:" + divide);
+        }
+    }
+}
